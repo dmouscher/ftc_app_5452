072c2019604066c297c79ff3ff8c8373904b3402
Daniel M <dmqwerty425@gmail.com>
2015-10-13 17:16:24 -0500

    Updated FtcOpModeRegister.java to 15.10.06.002

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -52,7 +52,7 @@ public class FtcOpModeRegister implements OpModeRegister
 	{
 
     /*
-	 * register your op modes here.
+     * register your op modes here.
      * The first parameter is the name of the op mode
      * The second parameter is the op mode class property
      *
@@ -63,18 +63,13 @@ public class FtcOpModeRegister implements OpModeRegister
 
 		//manager.register("NullOp", NullOp.class);
 
+		//manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
 		//manager.register("K9TeleOp", K9TeleOp.class);
 		//manager.register ("PushBotAuto", PushBotAuto.class);
 		//manager.register ("PushBotManual", PushBotManual.class);
 
-    /*
-	 * Uncomment any of the following lines if you want to register an op mode.
-     */
-
 		//manager.register("AdafruitRGBExample", AdafruitRGBExample.class);
-		//manager.register("MRRGBExample", MRRGBExample.class);
 		//manager.register("ColorSensorDriver", ColorSensorDriver.class);
-		//manager.register("HTRGBExample", HTRGBExample.class);
 
 		//manager.register("IrSeekerOp", IrSeekerOp.class);
 		//manager.register("CompassCalibration", CompassCalibration.class);
@@ -94,8 +89,8 @@ public class FtcOpModeRegister implements OpModeRegister
 		//manager.register ("PushBotOdsFollowEvent", PushBotOdsFollowEvent.class);
 		//manager.register ("PushBotTouchEvent", PushBotTouchEvent.class);
 
-		//manager.register("PushBotDriveTouch", PushBotDriveTouch.class);
-		//manager.register("PushBotIrSeek", PushBotIrSeek.class);
-		//manager.register("PushBotSquare", PushBotSquare.class);
+		//manager.register("PushBotDriveTouch", PushBotDriveTouch.java);
+		//manager.register("PushBotIrSeek", PushBotIrSeek.java);
+		//manager.register("PushBotSquare", PushBotSquare.java);
 	}
 }
