c56fc882c0c151508eb1f919601e09e3777486bb
Daniel M <dmqwerty425@gmail.com>
2015-10-21 20:55:50 -0500

    Modified Artreads to work with robot
    Numerous small changes to get the op mode to work with the robot as well
    as cleaning up the code by stuffing most of it into several different
    methods.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/custom/Artreads.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/custom/Artreads.java
@@ -5,8 +5,8 @@ This is a one-gamepad teleop op mode is meant for testing the prototype drive tr
 
 • The drive motors can be controlled in the traditional way using the joysticks
 • Otherwise, the y-button moves the robot forwards and the a-button moves the robot backwards both at the set speed
-• The left trigger button moves the left motor forwards and the right trigger button moves the right motor forwards both at the set speed
-• The trigger buttons' movement can be reversed by holding down the b-button
+• The left bumper button moves the left motor forwards and the right trigger button moves the right motor forwards both at the set speed
+• The bumper buttons' movement can be reversed by holding down the b-button
 • The set speed can be increased by holding down "up" on the dpad and can be decreased by holding down "down" on the dpad
 
  */
@@ -15,7 +15,7 @@ package com.qualcomm.ftcrobotcontroller.opmodes.custom;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
+import java.lang.*;
 
 public class Artreads extends LinearOpMode
 {
@@ -24,81 +24,85 @@ public class Artreads extends LinearOpMode
 	DcMotor motorBL;
 	DcMotor motorBR;
 
-	public int reverseMultiplier()
+	boolean isPrimed = true;
+	double speed = 0.5;
+	final double DEADZONE = 0.2;
+
+	private int reverseMultiplier()
 	{
-		if (gamepad1.b)
-		{
-			return -1;
-		}
-		else
-		{
-			return 1;
-		}
+		if (gamepad1.b) { return -1; }
+		           else { return  1; }
 	}
+
+	private void runAllMotors(double speed)
+	{
+		motorFL.setPower(speed);
+		motorBL.setPower(speed);
+		motorFR.setPower(speed);
+		motorBR.setPower(speed);
+	}
+
+	private void runLeftMotors(double speed)
+	{
+		motorFL.setPower(speed);
+		motorBL.setPower(speed);
+	}
+
+	private void runRightMotors(double speed)
+	{
+		motorFR.setPower(speed);
+		motorBR.setPower(speed);
+	}
+
+	private boolean areJoysticksActive() { return (Math.abs(gamepad1.left_stick_y) > DEADZONE || Math.abs(gamepad1.right_stick_y) > DEADZONE); }
 
 	@Override
 	public void runOpMode() throws InterruptedException
 	{
-		motorFL = hardwareMap.dcMotor.get("motor_fl");
-		motorFR = hardwareMap.dcMotor.get("motor_fr");
-		motorBL = hardwareMap.dcMotor.get("motor_bl");
-		motorBR = hardwareMap.dcMotor.get("motor_br");
+		motorFL = hardwareMap.dcMotor.get("fl");
+		motorFR = hardwareMap.dcMotor.get("fr");
+		motorBL = hardwareMap.dcMotor.get("bl");
+		motorBR = hardwareMap.dcMotor.get("br");
 
-		motorFL.setDirection(DcMotor.Direction.REVERSE);
 		motorFR.setDirection(DcMotor.Direction.REVERSE);
-
-		double speed = 0.5;
-		final double DEADZONE = 0.05;
+		motorBR.setDirection(DcMotor.Direction.REVERSE);
 
 		waitForStart();
 
-		while (opModeIsActive())
+		while(opModeIsActive())
 		{
-			if (gamepad1.left_stick_y < DEADZONE || gamepad1.right_stick_y < DEADZONE)
+			if (areJoysticksActive())
 			{
-				motorFL.setPower(gamepad1.left_stick_y);
-				motorBL.setPower(gamepad1.left_stick_y);
-				motorFR.setPower(gamepad1.right_stick_y);
-				motorBR.setPower(gamepad1.right_stick_y);
+				runLeftMotors(gamepad1.left_stick_y);
+				runRightMotors(gamepad1.right_stick_y);
 			}
 			else
 			{
-				if (gamepad1.left_bumper)
-				{
-					motorFL.setPower(speed * reverseMultiplier());
-					motorBL.setPower(speed * reverseMultiplier());
-				}
-				if (gamepad1.right_bumper)
-				{
-					motorFR.setPower(speed * reverseMultiplier());
-					motorBR.setPower(speed * reverseMultiplier());
-				}
-				if (gamepad1.y)
-				{
-					motorFL.setPower(speed);
-					motorBL.setPower(speed);
-					motorFR.setPower(speed);
-					motorBR.setPower(speed);
-				}
-				if (gamepad1.a)
-				{
-					motorFL.setPower(-speed);
-					motorBL.setPower(-speed);
-					motorFR.setPower(-speed);
-					motorBR.setPower(-speed);
-				}
+				runAllMotors(0);
+
+				if  (gamepad1.left_bumper) {  runLeftMotors(speed * reverseMultiplier()); }
+				if (gamepad1.right_bumper) { runRightMotors(speed * reverseMultiplier()); }
+
+				if (gamepad1.y) { runAllMotors( speed); }
+				if (gamepad1.a) { runAllMotors(-speed); }
 			}
 
-			if (gamepad1.dpad_up && speed < 1)
+			if (gamepad1.dpad_up && speed < 0.95 && isPrimed)
 			{
-				speed += 0.005;
+				speed += 0.05;
+				isPrimed = false;
 			}
-			else if (gamepad1.dpad_down && speed > 0)
+			else if (gamepad1.dpad_down && speed > 0.05 && isPrimed)
 			{
-				speed -= 0.005;
+				speed -= 0.05;
+				isPrimed = false;
 			}
 
-			telemetry.addData("Speed: ", speed * 100 + "%");
+			if(!(gamepad1.dpad_up && gamepad1.dpad_down && isPrimed)) { isPrimed = true; }
+
+			telemetry.addData("Speed: ", (int)(speed*100) + "%");
+			telemetry.addData("Bumpers: ", gamepad1.left_bumper + ", " + gamepad1.right_bumper);
+			telemetry.addData("Joysticks: ", areJoysticksActive() ? "Active" : "Inactive");
 			waitOneFullHardwareCycle();
 		}
 	}
