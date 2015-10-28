0e71e076607e870700fe3664c78b174e74f16b7d
Max Lowery <brainlessml@gmail.com>
2015-10-28 16:32:40 -0500

    :tropical_fish: Trying to find a good way to reset the encoders
    still don't have one

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/custom/EncoderTesting.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/custom/EncoderTesting.java
@@ -8,28 +8,31 @@ import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 public class EncoderTesting extends OpMode {
-    public EncoderTesting(){}
      DcMotor testmotor;
+     double finalEncoderValue;
+     double currentEncoderValue;
+
+    public EncoderTesting()
+    {
+        finalEncoderValue = 0;
+        currentEncoderValue = 0;
+    }
 
     @Override
      public void init()
      {
          hardwareMap.logDevices();
          testmotor = hardwareMap.dcMotor.get("test");
-         testmotor.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
+         //testmotor.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
          testmotor.setChannelMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
      }
 
     @Override
-    public void start()
-    {
-    }
-
-    @Override
     public void loop()
     {
-        telemetry.addData("", testmotor.getCurrentPosition());
-        if(testmotor.getCurrentPosition() <= 1440)
+        currentEncoderValue = testmotor.getCurrentPosition() - finalEncoderValue; // This is if because the encoder value carrys over between starts and stops of programs. And the only way to fully reset it is to cycle the power. :grin: I think
+        telemetry.addData("", currentEncoderValue);
+        if(currentEncoderValue <= 1440)
         {
             testmotor.setPower(0.25);
         }
@@ -42,6 +45,6 @@ public class EncoderTesting extends OpMode {
     @Override
     public void stop()
     {
-
+        finalEncoderValue += testmotor.getCurrentPosition();
     }
 }
