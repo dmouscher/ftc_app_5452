3f6cac7e0fa78f0b28dadbbb869cbd1836a9d6f5
Daniel M <dmqwerty425@gmail.com>
2015-11-04 15:40:09 -0600

    Added Skeleton Op Modes
    Added some bare-bones op modes so we can more easily create new ones.

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/custom/LinearSkeleton.java
@@ -0,0 +1,12 @@
+package com.qualcomm.ftcrobotcontroller.opmodes.custom;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+public class LinearSkeleton extends LinearOpMode
+{
+	@Override
+	public void runOpMode() throws InterruptedException
+	{
+	}
+}
---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/custom/Skeleton.java
@@ -0,0 +1,28 @@
+package com.qualcomm.ftcrobotcontroller.opmodes.custom;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+public class Skeleton extends OpMode
+{
+	@Override
+	public void init()
+	{
+	}
+
+	@Override
+	public void start()
+	{
+	}
+
+	@Override
+	public void loop()
+	{
+	}
+
+	@Override
+	public void stop()
+	{
+	}
+}
