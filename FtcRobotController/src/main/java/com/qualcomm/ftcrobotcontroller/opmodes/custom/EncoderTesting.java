d4cb402c90f28608d380722b600f9a2e58dbb780
Max Lowery <brainlessml@gmail.com>
2015-11-05 16:44:28 -0600

    :tongue: Better code to test encoders

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/custom/EncoderTesting.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/custom/EncoderTesting.java
@@ -8,9 +8,10 @@ import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 public class EncoderTesting extends OpMode {
-     DcMotor testmotor;
+     DcMotor testmotor, testmotor1;
      double finalEncoderValue;
      double currentEncoderValue;
+     boolean firstloop = true;
 
     public EncoderTesting()
     {
@@ -22,29 +23,37 @@ public class EncoderTesting extends OpMode {
      public void init()
      {
          hardwareMap.logDevices();
-         testmotor = hardwareMap.dcMotor.get("test");
-         //testmotor.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
-         testmotor.setChannelMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+         testmotor = hardwareMap.dcMotor.get("motor1");
+         testmotor1 = hardwareMap.dcMotor.get("motor2");
+         testmotor.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
      }
 
     @Override
     public void loop()
     {
-        currentEncoderValue = testmotor.getCurrentPosition() - finalEncoderValue; // This is if because the encoder value carrys over between starts and stops of programs. And the only way to fully reset it is to cycle the power. :grin: I think
-        telemetry.addData("", currentEncoderValue);
-        if(currentEncoderValue <= 1440)
+        if(firstloop)
         {
-            testmotor.setPower(0.25);
+            testmotor.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
+            firstloop = false;
         }
-        else
-        {
-            testmotor.setPower(0);
-        }
+
+        testmotor.setTargetPosition(1440);
+        testmotor.setPower(1);
+
+        /**
+         * So from here untill the end of the loop method, this code is meant for a very specific scenario but might be useful for later reference
+         * The scenario is that we want to run a second motor, without an encoder at the same speed as the first.
+         * And we are using the older motor controllers that used I2C and none of this fancy USB reading and writing at the same time.
+         *
+         * If this code is unneeded when using this program, comment out the lines untill the end of the void method and anything to do with testmotor1
+         */
+        //if(testmotor1.getMotorControllerDeviceMode() == DcMotorController.DeviceMode.values()[4]) // I think this is WRITE_ONLY and 0 is READ_ONLY (according to ftc_app_5452/doc/javadoc/com/qualcomm/robotcore/hardware/DcMotorController.DeviceMode.html)
+        //testmotor1.setPower(testmotor.getPower());
     }
 
     @Override
     public void stop()
     {
-        finalEncoderValue += testmotor.getCurrentPosition();
+
     }
 }
