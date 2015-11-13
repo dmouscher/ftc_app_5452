eb93ec5b54a85998f341becb8d434a051115f5c4
Daniel M <dmqwerty425@gmail.com>
2015-11-13 09:04:43 -0600

    Unregistered some op modes

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -61,10 +61,10 @@ public class FtcOpModeRegister implements OpModeRegister
      *
      * If two or more op modes are registered with the same name, the app will display an error.
      */
-		manager.register("Servo Test", ServoTest.class);
+
 		manager.register("Teleop", Teleop.class);
-		manager.register("Teleop (No Servos)", TeleopNoServos.class);
-		manager.register("Jackie Skeleton", Jackie_Skeleton.class);
+		//manager.register("Jackie Skeleton", Jackie_Skeleton.class);
+		//manager.register("Servo Test", ServoTest.class);
 		//manager.register("Color Sensor Driver", ColorSensorDriverModified.class);
 		//manager.register("Encoder Testing", EncoderTesting.class);
 		//manager.register("Color Sensor Testing", ColorSensorTesting.class);
