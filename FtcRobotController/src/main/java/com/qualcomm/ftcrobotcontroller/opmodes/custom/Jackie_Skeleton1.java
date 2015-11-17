77970426279bb9335887d1fdb3cc906598c365f0
catpat4 <192hirschj@student.dist113.org>
2015-11-17 15:30:30 -0600

    Fixing Errors

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/custom/Jackie_Skeleton1.java
@@ -0,0 +1,31 @@
+package com.qualcomm.ftcrobotcontroller.opmodes.custom;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+
+public class Jackie_Skeleton1 extends LinearOpMode
+{
+	DcMotor motor;
+	boolean firstloop = true;
+
+	@Override
+	public void runOpMode() throws InterruptedException
+	{
+		motor = hardwareMap.dcMotor.get("motor");
+		motor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+
+
+		waitForStart();
+		while (opModeIsActive())
+
+		{
+			motor.setPower(0.8);
+			if (firstloop)
+		}
+		{
+			motor.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+			firstloop = false;
+		}
+	}
+}
