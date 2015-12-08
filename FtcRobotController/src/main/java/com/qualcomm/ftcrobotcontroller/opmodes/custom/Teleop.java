8eca6015364ce10362ab489367c1a4d11e6a6f6c
Daniel M <dmqwerty425@gmail.com>
2015-12-08 16:34:32 -0600

    Added and removed telemetry data

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/custom/Teleop.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/custom/Teleop.java
@@ -129,13 +129,13 @@ public class Teleop extends LinearOpMode
 				telemetry.addData("Joysticks", gamepad1.left_stick_y + ", " + gamepad1.right_stick_y);
 
 				telemetry.addData("Buttons 1", (gamepad1.y            ? "[1Y] "  : "") + (gamepad1.b            ? "[1B] "  : "") +
-						                       (gamepad1.left_bumper  ? "[1LB] " : "") + (gamepad1.right_bumper ? "[1RB] " : "") +
-						                       (isTriggered(1, Direction.RIGHT) ? "[1RT] " : "")); //Update when button usage changes
+						                       (isTriggered(1, Direction.LEFT) ? "[1LT] " : "")); //Update when button usage changes
 
-				telemetry.addData("Buttons 2", (gamepad2.a            ? "[2A] "  : "") + (gamepad2.b           ? "[2B] "  : "") +
-						                       (gamepad2.y            ? "[2Y] "  : "") + (gamepad2.left_bumper ? "[2LB] " : "") +
-						                       (gamepad2.right_bumper ? "[2RB] " : "") + (gamepad1.dpad_up     ? "[1DU] " : "") +
-						                       (gamepad1.dpad_down    ? "[1DD] " : "") +
+				telemetry.addData("Buttons 2", (gamepad2.a            ? "[2A] "  : "") + (gamepad2.b            ? "[2B] "  : "") +
+						                       (gamepad2.x            ? "[2A] "  : "") + (gamepad2.y            ? "[2Y] "  : "") +
+						                       (gamepad2.left_bumper ? "[2LB] "  : "") + (gamepad2.right_bumper ? "[2RB] " : "") +
+						                       (gamepad1.dpad_up     ? "[1DU] "  : "") + (gamepad1.dpad_down    ? "[1DD] " : "") +
+						                       (gamepad1.dpad_left   ? "[1DL] "  : "") + (gamepad1.dpad_right   ? "[1DR] " : "") +
 						                       (isTriggered(2, Direction.LEFT ) ? "[2LT] " : "") +
 						                       (isTriggered(2, Direction.RIGHT) ? "[2RT] " : "")); //Update when button usage changes
 
