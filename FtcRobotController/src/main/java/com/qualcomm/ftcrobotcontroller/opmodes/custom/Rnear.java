a3453f949aa4cb8ff84c35ca2fb8951aa3df96bb
catpat4 <192hirschj@student.dist113.org>
2016-01-06 22:13:37 -0600

    Created Java Class For Red Close (Near)

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/custom/Rnear.java
@@ -0,0 +1,16 @@
+package com.qualcomm.ftcrobotcontroller.opmodes.custom;
+
+/**
+ * Created by jackiehirsch on 1/6/16.
+ */
+public class Rnear {
+
+}
+{
+@Override
+public void runOpMode() throws InterruptedException
+        {
+        DcMotor driveLeft;
+        DcMotor driveRight;
+        }
+        }
