900261fd56c75274621fa6bf6ed4cd060c5f713a
catpat4 <192hirschj@student.dist113.org>
2016-01-09 16:42:58 -0600

    Added Names and Started To Write for Move Forward

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/custom/Rnear.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/custom/Rnear.java
@@ -6,11 +6,34 @@ package com.qualcomm.ftcrobotcontroller.opmodes.custom;
 public class Rnear {
 
 }
+
 {
 @Override
 public void runOpMode() throws InterruptedException
         {
         DcMotor driveLeft;
         DcMotor driveRight;
+
+        DcMotor armRotate;
+        DcMotor armExtend;
+
+        DcMotor plow;
+
+        Servo dropperBase;
+        Servo dropperJoint;
+        Servo rescueLeft;
+        Servo rescueRight;
+
+        motor = hardwareMap.dcMotor.get("motor");
+
+        motor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+
+        motor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+
+        waitForStart();
+
+        telemetry.addData("Phase 1", "");
+        moveForward(1440, 0.8, 1000);
+
         }
         }
