1549651e5d0038c504ed72946576263570095cd5
Daniel M <dmqwerty425@gmail.com>
2016-01-11 15:35:18 -0600

    Reversed direction of plow in teleop
    Teleop experience remains unchanged. Also fixed the spacing for
    telemetry

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/custom/Teleop.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/custom/Teleop.java
@@ -35,7 +35,7 @@ public class Teleop extends LinearOpMode
 	final double  BASE_SPEED            = 0.005 ;
 	final double  JOINT_SPEED           = 0.010 ;
 	final double  PLOW_SPEED            = 0.500 ;
-	final boolean TELEMETRY             = true  ; //enables/disables telemetry
+	final boolean TELEMETRY             = true  ; //toggles telemetry
 
 	double driveSlowMultiplier = 1;
 
@@ -64,7 +64,7 @@ public class Teleop extends LinearOpMode
 
 		driveRight.setDirection(DcMotor.Direction.REVERSE);
 		rescueRight.setDirection(Servo.Direction.REVERSE);
-
+        plow.setDirection(DcMotor.Direction.REVERSE);
 		gamepad1.setJoystickDeadzone(DEADZONE);
 		gamepad2.setJoystickDeadzone(DEADZONE);
 
@@ -95,7 +95,7 @@ public class Teleop extends LinearOpMode
 			if(gamepad2.y ^ gamepad2.b) { armExtend.setPower(EXTEND_SPEED * (gamepad2.y ? 1 : -1)); } //todo: readd comments for printout
 			else { armExtend.setPower(0); } //todo: add an encoder limit for this conditional
 
-			if(gamepad2.x ^ gamepad2.a) { plow.setPower(PLOW_SPEED * (gamepad2.a ? 1 : -1)); } //todo: add comments for printout
+			if(gamepad2.x ^ gamepad2.a) { plow.setPower(PLOW_SPEED * (gamepad2.x ? 1 : -1)); } //todo: add comments for printout
 			else { plow.setPower(0); } //todo: add an encoder limit for this conditional
 
 			if(gamepad1.y ^ gamepad1.b) //gamepad1.y moves the robot straight and forwards, gamepad1.b moves it straight and backwards
@@ -128,14 +128,14 @@ public class Teleop extends LinearOpMode
 			{
 				telemetry.addData("Joysticks", gamepad1.left_stick_y + ", " + gamepad1.right_stick_y);
 
-				telemetry.addData("Buttons 1", (gamepad1.y            ? "[1Y] "  : "") + (gamepad1.b            ? "[1B] "  : "") +
+				telemetry.addData("Buttons 1", (gamepad1.y           ? "[1Y] "  : "") + (gamepad1.b            ? "[1B] "  : "") +
 						                       (isTriggered(1, Direction.LEFT) ? "[1LT] " : "")); //Update when button usage changes
 
-				telemetry.addData("Buttons 2", (gamepad2.a            ? "[2A] "  : "") + (gamepad2.b            ? "[2B] "  : "") +
-						                       (gamepad2.x            ? "[2A] "  : "") + (gamepad2.y            ? "[2Y] "  : "") +
-						                       (gamepad2.left_bumper ? "[2LB] "  : "") + (gamepad2.right_bumper ? "[2RB] " : "") +
-						                       (gamepad1.dpad_up     ? "[1DU] "  : "") + (gamepad1.dpad_down    ? "[1DD] " : "") +
-						                       (gamepad1.dpad_left   ? "[1DL] "  : "") + (gamepad1.dpad_right   ? "[1DR] " : "") +
+				telemetry.addData("Buttons 2", (gamepad2.a           ? "[2A] "  : "") + (gamepad2.b            ? "[2B] "  : "") +
+						                       (gamepad2.x           ? "[2A] "  : "") + (gamepad2.y            ? "[2Y] "  : "") +
+						                       (gamepad2.left_bumper ? "[2LB] " : "") + (gamepad2.right_bumper ? "[2RB] " : "") +
+						                       (gamepad1.dpad_up     ? "[1DU] " : "") + (gamepad1.dpad_down    ? "[1DD] " : "") +
+						                       (gamepad1.dpad_left   ? "[1DL] " : "") + (gamepad1.dpad_right   ? "[1DR] " : "") +
 						                       (isTriggered(2, Direction.LEFT ) ? "[2LT] " : "") +
 						                       (isTriggered(2, Direction.RIGHT) ? "[2RT] " : "")); //Update when button usage changes
 
@@ -152,12 +152,12 @@ public class Teleop extends LinearOpMode
 
 	private void runAllMotors(double speed) //simply runs all drivetrain motors at the given speed
 	{
-		driveLeft.setPower(speed);
+		driveLeft .setPower(speed);
 		driveRight.setPower(speed);
 	}
 
 	public boolean isTriggered(int gamepad, Direction dir) //returns true if the given trigger has been pressed past the threshold constant
-	{                                                //otherwise returns false
+	{                                                      //otherwise returns false
 		if(gamepad == 1) { return (dir == Direction.LEFT ? gamepad1.left_trigger : gamepad1.right_trigger) > TRIGGER_THRESHOLD; }
 		else             { return (dir == Direction.LEFT ? gamepad2.left_trigger : gamepad2.right_trigger) > TRIGGER_THRESHOLD; }
 	}
