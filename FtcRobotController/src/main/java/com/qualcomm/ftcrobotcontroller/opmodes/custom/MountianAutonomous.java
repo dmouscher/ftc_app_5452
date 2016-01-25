abdedcda2a4dd4a27bb027b1d7eedc1a4eeb08a7
catpat4 <192hirschj@student.dist113.org>
2016-01-25 17:05:56 -0600

    Started Mountain Autonomous

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/custom/MountianAutonomous.java
@@ -0,0 +1,16 @@
+package com.qualcomm.ftcrobotcontroller.opmodes.custom;
+
+
+        import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+        import com.qualcomm.robotcore.hardware.DcMotor;
+        import com.qualcomm.robotcore.hardware.DcMotorController;
+        import com.qualcomm.robotcore.hardware.Servo;
+        import com.qualcomm.robotcore.robocol.Telemetry;
+        import com.qualcomm.robotcore.util.Range;
+        import java.lang.Math;
+
+/**
+ * Created by jackiehirsch on 1/25/16.
+ */
+public class MountianAutonomous {
+}
