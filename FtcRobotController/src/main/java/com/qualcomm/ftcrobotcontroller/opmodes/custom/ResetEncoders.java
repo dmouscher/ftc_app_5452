c3bdc1889c9418a8e22529999802cbfc6e58fc8e
Daniel M <dmqwerty425@gmail.com>
2016-01-27 20:37:37 -0600

    Converted ResetEncoders op mode from OpMode to LinearBase

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/custom/ResetEncoders.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/custom/ResetEncoders.java
@@ -3,57 +3,14 @@
 
 package com.qualcomm.ftcrobotcontroller.opmodes.custom;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
-import com.qualcomm.robotcore.hardware.Servo;
 
-
-public class ResetEncoders extends OpMode
+public class ResetEncoders extends LinearBase
 {
-	DcMotor driveLeft;
-	DcMotor driveRight;
-
-	DcMotor armRotate;
-	DcMotor armExtend;
-
-	DcMotor plow;
-
-	Servo dropperBase;
-
-	Servo rescueLeft;
-	Servo rescueRight;
-
 	@Override
-	public void init()
+	public void runOpMode() throws InterruptedException
 	{
-		driveLeft  = hardwareMap.dcMotor.get("left" );
-		driveRight = hardwareMap.dcMotor.get("right");
-
-		armRotate = hardwareMap.dcMotor.get("rotate");
-		armExtend = hardwareMap.dcMotor.get("extend");
-
-		plow = hardwareMap.dcMotor.get("plow");
-
-		dropperBase = hardwareMap.servo.get("base");
-
-		rescueLeft  = hardwareMap.servo.get("rql");
-		rescueRight = hardwareMap.servo.get("rqr");
-
-		driveLeft .setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
-		driveRight.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+		waitForStart();
+		while(opModeIsActive()) { drivetrainSetMode(DcMotorController.RunMode.RESET_ENCODERS); }
 	}
-
-	@Override
-	public void start() {}
-
-	@Override
-	public void loop()
-	{
-		driveLeft .setMode(DcMotorController.RunMode.RESET_ENCODERS);
-		driveRight.setMode(DcMotorController.RunMode.RESET_ENCODERS);
-	}
-
-	@Override
-	public void stop() {}
 }
