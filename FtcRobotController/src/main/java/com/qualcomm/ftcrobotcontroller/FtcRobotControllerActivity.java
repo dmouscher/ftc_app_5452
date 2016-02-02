1031cfbadb19010618879159c37f0e1983cfe4b4
Daniel M <dmqwerty425@gmail.com>
2016-02-02 17:50:44 -0600

    Updated to ftc_app Release 20160104

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/FtcRobotControllerActivity.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/FtcRobotControllerActivity.java
@@ -40,6 +40,7 @@ import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.hardware.usb.UsbManager;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
@@ -81,6 +82,7 @@ public class FtcRobotControllerActivity extends Activity
 
 	public static final String CONFIGURE_FILENAME = "CONFIGURE_FILENAME";
 
+	protected WifiManager.WifiLock wifiLock;
 	protected SharedPreferences preferences;
 
 	protected UpdateUI.Callback callback;
@@ -182,6 +184,9 @@ public class FtcRobotControllerActivity extends Activity
 		PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
 		preferences = PreferenceManager.getDefaultSharedPreferences(this);
 
+		WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+		wifiLock = wifiManager.createWifiLock(WifiManager.WIFI_MODE_FULL_HIGH_PERF, "");
+
 		hittingMenuButtonBrightensScreen();
 
 		if (USE_DEVICE_EMULATION) { HardwareFactory.enableDeviceEmulation(); }
@@ -212,6 +217,7 @@ public class FtcRobotControllerActivity extends Activity
 			}
 		});
 
+		wifiLock.acquire();
 	}
 
 	@Override
@@ -234,6 +240,8 @@ public class FtcRobotControllerActivity extends Activity
 		if (controllerService != null) unbindService(connection);
 
 		RobotLog.cancelWriteLogcatToDisk(this);
+
+		wifiLock.release();
 	}
 
 	@Override
