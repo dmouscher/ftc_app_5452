e5e8a2f22df2631a98f6ad971cd8a31f96257f6a
Daniel M <dmqwerty425@gmail.com>
2016-02-24 15:30:34 -0600

    Added movePlow() calls to mountain autonomi

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DMNR.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DMNR.java
@@ -11,7 +11,7 @@ public class DMNR extends LinearBase
         initialize();
         waitForStart();
 
-
+        movePlow(0.75, PLOW_EXTEND_LENGTH);
         moveTarget((int)(42*IN /* 12 ft times the distance you want.*/), 0.8, 1000);
         turn(-135, 0.8, 1000); /* Make sure this turns left */
         moveTarget((int)(18*IN /* 12 ft times the distance you want. Also used 12 to turn inches into feet. */), 0.8, 1000);
